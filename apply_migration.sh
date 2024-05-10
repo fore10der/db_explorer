c29d55953758192856d30bd79b1ac4eda93a60de
fore10der <qovalski@gmail.com>
2024-05-10 18:04:41 +0600

    apply_migrations

--- apply_migration.sh
+++ apply_migration.sh
@@ -1,1 +1,1 @@
-docker compose exec -T dbMysql mariadb -uroot -plove photolist < db_explorer/_mysql/sample_db.sql
+docker compose exec -T dbMysql mariadb -uroot -plove photolist < _mysql/sample_db.sql
